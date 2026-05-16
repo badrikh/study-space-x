19a1052cd7c546e991e8c4e05b12d92b38ab0747
Ghaith Fouqhaa <122173873+Ghaihtfouqhaa122@users.noreply.github.com>
2026-05-16 13:38:19 +0300

    Refactor Menu model definition for consistency

--- models/menu.js
+++ models/menu.js
@@ -1,22 +1,16 @@
 export default (sequelize, DataTypes) => {
-  const Menu = sequelize.define(
-    "Menu",
-    {
-      name: DataTypes.STRING,
-      price: DataTypes.FLOAT,
-      itemNumber: DataTypes.STRING,
-      description: DataTypes.STRING,
-    },
-    {
-      tableName: "menus",
-    }
-  );
+    const Menu = sequelize.define(
+        "Menu",
+        {
+            name: DataTypes.STRING,
+            price: DataTypes.FLOAT,
+            itemNumber: DataTypes.STRING,
+            description: DataTypes.STRING,
+        },
+        {
+            tableName: "menus",
+        }
+    );
 
-  Menu.associate = (models) => {
-    Menu.hasMany(models.Order, {
-      foreignKey: "menu_id",
-    });
-  };
-
-  return Menu;
+    return Menu;
 };
