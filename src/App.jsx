8fcd30e13e298e43cf03dc44dc06cad822348c18
Ghaith Fouqhaa <122173873+Ghaihtfouqhaa122@users.noreply.github.com>
2026-05-17 01:41:33 +0300

    Delete src directory

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"; import Login from "./pages/Login"; import Booking from "./pages/Booking"; import CoffeeShop from "./pages/CoffeeShop"; import UserSubscriptions from "./pages/UserSubscriptions"; import Loyalty from "./pages/Loyalty"; import AdminDashboard from "./pages/admin/Dashboard"; import AdminOrders from "./pages/admin/Orders"; import AdminSubscriptions from "./pages/admin/Subscriptions"; import AdminAnalytics from "./pages/admin/Analytics";
-export default function App(){ return (<BrowserRouter><Routes>
-<Route path="/" element={<Home/>} /><Route path="/login" element={<Login/>} /><Route path="/booking" element={<Booking/>} /><Route path="/coffee" element={<CoffeeShop/>} /><Route path="/subs" element={<UserSubscriptions/>} /><Route path="/loyalty" element={<Loyalty/>} />
-<Route path="/admin" element={<AdminDashboard/>} /><Route path="/admin/orders" element={<AdminOrders/>} /><Route path="/admin/subscriptions" element={<AdminSubscriptions/>} /><Route path="/admin/analytics" element={<AdminAnalytics/>} />
-<Route path="*" element={<Navigate to="/" replace/>} /></Routes></BrowserRouter>)}
