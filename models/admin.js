15124f2ba5912f6f4be98433e0e7a2a8d5bbf75a
Ghaith Fouqhaa <122173873+Ghaihtfouqhaa122@users.noreply.github.com>
2026-05-16 13:35:54 +0300

    Define Admin model with Sequelize

--- models/admin.js
+++ models/admin.js
@@ -1,1 +1,17 @@
-
+export default (sequelize, DataTypes) => {
+    const Admin = sequelize.define(
+        "Admin",
+        {
+            name: DataTypes.STRING,
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            password: DataTypes.STRING,
+        },
+        {
+            tableName: "admins",
+        }
+    );
+    return Admin;
+};
