acb686e196b9bacd1cd99d70ad8ac66740497815
Raghad12217718 <153433795+Raghad12217718@users.noreply.github.com>
2026-05-16 19:05:21 +0300

    Update booking.js

--- study-space-x-backend/models/booking.js
+++ study-space-x-backend/models/booking.js
@@ -2,11 +2,13 @@ export default (sequelize, DataTypes) => {
   const Booking = sequelize.define(
     "Booking",
     {
-      date: DataTypes.DATEONLY,
+      date: DataTypes.DATE,  // change from DATEONLY to DATE,
       userId: DataTypes.INTEGER,
       seatId: DataTypes.INTEGER,
       timeSlotId: DataTypes.INTEGER,
-    },
+      people: DataTypes.INTEGER,       // ADD THIS
+      duration: DataTypes.INTEGER   // ADD THIS
+        },
     {
       tableName: "bookings",
     }
